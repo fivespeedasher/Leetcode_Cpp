8488d71df37d7539835017a8f6e73fcdb9905296
Fivespeedasher <joriohyang@outlook.com>
2024-06-11 10:06:56 +0800

    209-23%

--- leetcode/Array/209/209.cpp
+++ leetcode/Array/209/209.cpp
@@ -5,8 +5,8 @@ using namespace std;
 class Solution {
     public:
         //题目：查找大于等于s的长度最小的子数组
-        int minSubArrayLen(int s, vector<int>& nums) {
-            // 暴力
+        // 暴力
+        int minSubArrayLen1(int s, vector<int>& nums) {
             int left = 0;
             int min_len = nums.size() + 1;
             int subLength, sum = 0;
@@ -24,12 +24,28 @@ class Solution {
             if(min_len == nums.size() + 1) return 0;
             else return  min_len;
             }
-        };
+        
+        // 滑窗：关键在于只让末指针循环
+        int minSubArrayLen2(int s, vector<int>& nums) {
+            int left = 0, right = 0, sum = 0, min_len = nums.size() + 1;
+            for(;right < nums.size(); right++) {
+                sum += nums[right];
+                while(sum - nums[left] >= s) {
+                    sum -= nums[left];
+                    left++;
+                }
+                if(sum >= s) min_len = min_len < (right - left + 1) ? min_len :  (right - left + 1);
+            }
+            return min_len == nums.size() + 1 ? 0 : min_len;
+            // if(min_len < nums.size()-1) return min_len;
+            // else return 0;
+        }
+    };
 
 int main() {
     Solution s1;
    int s = 7;
     vector<int> nums = {2,3,1,2,4,3};
-    int min_len = s1.minSubArrayLen(s, nums);
+    int min_len = s1.minSubArrayLen2(s, nums);
     cout << min_len << endl;
-}
+};
