c6842baa990fb1019159d0df440149e8d128cb29
Fivespeedasher <joriohyang@outlook.com>
2024-06-11 14:47:05 +0800

    059-13%

---
+++ leetcode/Array/059/059.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+    public:
+    //题目：生成螺旋矩阵
+        vector<vector<int>> generateMatrix(int n) {
+            vector<vector<int>> res(n, vector<int>(n, 0));
+            //定义左闭右开的边界
+            int start_i = 0, start_j = 0, offset = 1, loop = n/2;
+            int count = 1;
+            int i, j;
+            while(loop--) {
+                // 上边
+                int j = start_j, i = start_i;
+                for(; j < (n - offset); j++) { //左闭右开,每次遍历都是最后一个不处理
+                    res[i][j] = count++;
+                }
+                // 右边
+                for(i; i < (n - offset); i++) {
+                    res[i][n - offset] = count++;
+                }
+                // 下边
+                for(; j > start_j; j--) {
+                    res[n - offset][j] = count++;
+                }
+                // 左边
+                for(; i > start_i; i--) {
+                    res[i][start_j] = count++;
+                }
+
+                start_i++;
+                start_j++;
+                offset++;
+            }
+            // 处理剩余的中心点的情况
+            if(n % 2) {
+                res[n/2][n/2] = count;
+            }
+            return res;
+        }
+};
+
+int main() {
+    Solution s;
+    int n = 5;
+    vector<vector<int>> res = s.generateMatrix(n);
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cout << res[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
