6ae7b213e550a390c86cd0927286845a3eb5aca8
Fivespeedasher <joriohyang@outlook.com>
2024-06-17 14:54:27 +0800

    206-双指针

---
+++ leetcode/LinkedList/206/206.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x): val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head == NULL || head->next == NULL) return head;
+        ListNode* pre = NULL;
+        ListNode* cur = head;
+        while(cur){
+            ListNode* temp = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = temp;
+        }
+        return pre;
+    }
+};
+
+int main() {
+    //build a linked list
+    int num;
+    ListNode* head  = new ListNode(0);
+    ListNode* p  = head;
+    while(cin >> num) {
+        ListNode* temp = new ListNode(num);
+        p->next =temp;
+        p = temp;
+    }
+
+    Solution s;
+    head= s.reverseList(head);
+
+    //print the linked list
+    p = head;
+    while(p) {
+        cout << p->val << " ";
+        p = p->next;
+    }
+}
